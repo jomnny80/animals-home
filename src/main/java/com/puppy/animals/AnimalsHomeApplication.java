f32a8a77173d0d21f52fda42c2cf36f860695e8d
zong-xian <jomnny80@gmail.com>
2022-04-24 23:07:20 +0800

    request mapping

--- src/main/java/com/puppy/animals/AnimalsHomeApplication.java
+++ src/main/java/com/puppy/animals/AnimalsHomeApplication.java
@@ -2,12 +2,13 @@ package com.puppy.animals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AnimalsHomeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AnimalsHomeApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(AnimalsHomeApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/puppy/animals/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.puppy.animals.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/user")
+public class HelloController {
+
+//    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @GetMapping
+    public String getUser(){
+        return "Get user";
+    }
+
+//    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    @PostMapping
+    public String saveUser(){
+        return "Post user";
+    }
+
+//    @RequestMapping(value = "/user", method = RequestMethod.PUT)
+    @PutMapping
+    public String putUser(){
+        return "Put user";
+    }
+
+//    @RequestMapping(value = "/user", method = RequestMethod.DELETE)
+    @DeleteMapping
+    public String deleteUser(){
+        return "Delete user";
+    }
+}
