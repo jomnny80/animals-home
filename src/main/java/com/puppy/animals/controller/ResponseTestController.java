f30dc000ba1fc8432d4a0607d046ed6ddcf754e4
zong-xian <jomnny80@gmail.com>
2022-05-05 09:12:37 +0800

    http message converter

--- src/main/java/com/puppy/animals/controller/ResponseTestController.java
+++ src/main/java/com/puppy/animals/controller/ResponseTestController.java
@@ -1,6 +1,7 @@
 package com.puppy.animals.controller;
 
 import com.puppy.animals.bean.Person;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,7 +11,15 @@ import java.util.Date;
 @Controller
 public class ResponseTestController {
 
-    @ResponseBody
+    @ResponseBody // RequestResponseBodyMethodProcessor ---> messageConverter
+    @GetMapping("/file")
+    public FileSystemResource file(){
+
+        //文件以這樣的方式返回，看是誰處理的
+        return null;
+    }
+
+    @ResponseBody // 利用返回值處理器裡面的消息轉換器進行處理
     @GetMapping("/test/person")
     public Person getPerson(){
         Person person = new Person();
